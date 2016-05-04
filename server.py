b692a041bc7e555a4e261fcc68ab310a0291675d
Hackbright Student <info@hackbrightacademy.com>
2016-05-03 18:07:59 -0700

    Attempting to add user login information

--- server.py
+++ server.py
@@ -27,6 +27,42 @@ def index():
     return render_template("homepage.html")
 
 
+@app.route('/users')
+def user_list():
+    """Show list of users"""
+
+    users = User.query.all()
+    return render_template('user_list.html', users=users)
+
+@app.route('/login_form', methods=['GET', 'POST'])
+def login():
+    """Users login template"""
+
+    return render_template('user_login_form.html')
+
+
+@app.route('/process_login', methods=['GET', 'POST'])
+def process_login():
+    #args returned as dictionary from the form
+    #pulling each and binding to variables for query to db
+    email = request.form["user-email"]
+    password = request.form["user-password"]
+    
+    #Debugging prints
+    # print (type(request.form))
+    # print("Email: ", email) 
+    # print("Password: ", password)
+    
+    # query the db-ratings for existence of the user
+    
+    user = User.query.filter_by(email = email).all()
+
+    
+        # print
+    return render_template("homepage.html")
+
+
+
 if __name__ == "__main__":
     # We have to set debug=True here, since it has to be True at the point
     # that we invoke the DebugToolbarExtension
